55f4b8beedb2752e7e496d498ce4b1dceb20278d
VicenteMedeiros <vicenteaml@hotmail.com>
2023-06-18 18:23:03 -0300

    Js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const weather = document.querySelectorAll('.main-div');
+window.addEventListener('load', () => {
+   let long;
+   let lat;
+
+   if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+    });
+   }
+});
